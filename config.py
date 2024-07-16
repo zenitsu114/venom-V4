231035ca452aca523c24ed7d697ab826091ac6f3
VENOMxCRAZYy <venomcrazy904@gmail.com>
2024-07-16 16:10:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWWhzLaUAhKj4J-DoZaLLV3KBH4")
+BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
 
   #Enter Your telegram username here without @
 OWNER_USERNAME=("venomXcrazy")
